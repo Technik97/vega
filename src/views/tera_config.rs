0723cf9c13b71c3fb897e81f4354e9133a1cb130
TechNik <aniket.97.mane@gmail.com>
2022-09-27 00:15:44 +0530

    Created tera config file

---
+++ src/views/tera_config.rs
@@ -0,0 +1,13 @@
+use tera::Tera;
+
+pub fn get_tera() -> Tera {
+    let tera = match Tera::new("templates/**/*.html") {
+        Ok(t) => t,
+        Err(e) => {
+            println!("Parsing error(s): {}", e);
+            ::std::process::exit(1);
+        }
+    };
+
+    tera
+}
